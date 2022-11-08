0a95732464836f65e3c7f5769f3c3cf805b74835
FacundoRivara <facundorivara18@gmail.com>
2022-11-08 15:47:55 -0300

    Avanzando con listeners y controlador de eventos

---
+++ app.js
@@ -0,0 +1,8 @@
+const btnAgregar = document.querySelector("[data-form-btn]");
+const input = document.querySelector("[data-form-input]");
+
+
+btnAgregar.addEventListener("click", function(){
+    const input = document.querySelector("[data-form-input]");
+    console.log(input.value);
+})
